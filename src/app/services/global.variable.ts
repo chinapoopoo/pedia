cf04413514b6e7ba74ace2daea9ed34f09f061ae
blue_lim <chinapoopoo@gmail.com>
2017-05-17 19:30:19 +0900

    카테고리

--- src/app/services/global.variable.ts
+++ src/app/services/global.variable.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from './http.services';
 
 @Injectable()
 export class GlobalVariableService {
-    constructor(){
+    category: Array<any> = [];
 
+    constructor(private http:HttpService){
+        this.getCategory();
     }
 
-    
+    getCategory() {
+        this.http.get('/category')
+        .subscribe(
+            data => {
+                this.category = data.json();
+            }
+        )
+    }
 }
