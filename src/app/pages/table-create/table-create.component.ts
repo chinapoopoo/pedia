8e4e7474c4c07c5c33656683c6068f2bbd0aa711
blue_lim <chinapoopoo@gmail.com>
2017-05-18 21:07:59 +0900

    최신

--- src/app/pages/table-create/table-create.component.ts
+++ src/app/pages/table-create/table-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { TableProvider } from '../../providers/table-provider';
 
 @Component({
   selector: 'table-creator',
@@ -7,10 +8,59 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TableCreateComponent implements OnInit {
   @Input() tableNo: number = -1;
+  head_list: Array<any> = [];
+  body_list: Array<any> = [];
+  newHead: string = '';
 
-  constructor() { }
+  constructor(private tableProvider: TableProvider) { }
 
   ngOnInit() {
+  
   }
 
+  getHead() {
+    this.tableProvider.getTableHead(this.tableNo)
+    .subscribe(
+      data => {
+        this.head_list = data.json();
+      }
+    )
+  }
+  addHead() {
+    this.tableProvider.addTableHead(this.tableNo, this.newHead)
+    .subscribe(
+      data => {
+        console.log('헤더 생성 완료.');
+        this.newHead = '';
+        this.getHead();
+      }
+    )
+  }
+  editHead(idx, order) {
+    this.tableProvider.editTableHead(this.tableNo, this.head_list[idx].name, order)
+    .subscribe(
+      data => {
+        console.log('헤더 수정 완료.');
+        this.getHead();
+      }
+    )
+  }
+  deleteHead(order) {
+    this.tableProvider.deleteTableHead(this.tableNo, order)
+    .subscribe(
+      data => {
+        console.log('헤더 삭제 완료.');
+        this.getHead();
+      }
+    )
+  }
+
+  getBody() {
+    this.tableProvider.getTableBody(this.tableNo)
+    .subscribe(
+      data => {
+        this.body_list = data.json();
+      }
+    )
+  }
 }
