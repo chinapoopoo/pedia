6ecb81ee13ea5ac7f133f8e05599b7097a905fe6
genesis <genesis041835@gmail.com>
2017-05-18 23:32:21 +0900

    asa

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.services';
 
 @Component({
   selector: 'app-welcome',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpService) { }
 
   ngOnInit() {
   }
